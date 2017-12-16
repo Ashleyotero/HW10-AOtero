a6eaea4bb6b1d9408adb8e946360c055b389521f
Ashley Otero <chun.jin.ling@gmail.com>
2017-12-16 10:01:04 -0700

    Final changes

--- HW10_AOtero.py
+++ HW10_AOtero.py
@@ -1,6 +1,9 @@
 # Ashley Otero - Homework 10
 # conda install flask (should be v0.12)
 from flask import Flask, request
+from pprint import pprint
+import urllib.parse
+import feedparser
 
 app = Flask("step2")
 
@@ -13,77 +16,63 @@ def main_page():
         <body>
           <h1>RSS Reader</h1>
           <form action="/result/" method="get">    <!-- /result/ is the URL to go to on submit -->
-             Enter a search term: 
+             Enter a search term:
             <input  type="text" name="search_term"
-                    value="" 
+                    value=""
                     size="40">
             <input type="submit" value="Submit">
-          </form>             
+          </form>
         </body>
       </html>
-    """      
+    """
 
 @app.route('/result/', methods=["GET"])
-def result_page():        
-    # request.args is a dict with the name and the value of the select and 
+def result_page():
+    # request.args is a dict with the name and the value of the select and
     print(request.args["search_term"])  # value for name = "search_term"  in input part of form
-    
-    import urllib.parse 
-    import feedparser    # must make sure the library is installed beforehand: conda install feedparser
-    from pprint import pprint # pretty print
-    
+
     #search_term = "Penny Arcade" # testing the search_term before giving it a user input
     #search_term = input("Search term? ")  # user input
     quoted_search_term = urllib.parse.quote(request.args["search_term"]) # make it ULR safe: space => %20, " => %22, etc.
     print("Searching for:", quoted_search_term)
-    
-    rss_feed_URL = 'http://search.live.com/results.aspx?q={:s}&format=rss'.format(quoted_search_term) # using Bing 
+
+    rss_feed_URL = 'http://search.live.com/results.aspx?q={:s}&format=rss'.format(quoted_search_term) # using Bing
     #print("RSS feed URLS is", rss_feed_URL)
-    
+
     # web-read the feed from URL
     f = feedparser.parse(rss_feed_URL)
-    
+
     # pretty print the entire parsed feed
-    #pprint(f) 
-    
+    #pprint(f)
+
     print_item_length = str(len(f.entries))
     print ("This feed has", len(f.entries), "items")
-    
-    # title, decription, date and link for all items
-        
-    class feedEntries:
-        def get_desc(e):
-            for e in f.entries:
-                #print(e.title, "<br>")
-                #print(e.description, "<br>")
-                #print(e.published, "<br>")
-                #print("<a href=", e.link, ">Link</a><br>")
-                #print()
-                #entry = e.title, "<br>"
-                #entry = entry + e.description, "<br>"
-                #entry = entry + e.published, "<br>"
-                #entry = entry + "<a href=", e.link, ">Link</a><br>"
 
-                entries = "<h1>", e.title, "</h1><p><em>", e.published, "</em></p><p>", e.description, "</p><a href=", e.link, ">Link</a><br>"                
-            return entries
-        
-    f = feedEntries().get_desc()
-    #print(f)
-     
+    # title, decription, date and link for all items
+
+    entries = ""
+    for e in f.entries:
+        entries = entries + "<div class='entry'><h1>{0}</h1><p><em>{1}</em></p><p>{2}</p><a href={3}>Link</a></div>".format(e.title, e.published, e.description, e.link)
+
     html = """
         <html>
+        <head>
+        <style>
+           body { font-family: sans-serif; }
+           .entry { background: #eee; padding: 20px; margin: 20px 0; }
+           h1 { border-bottom: 1px solid #ddd; color: #444; font-weight: 100; margin-top: 0px; }
+        </style>
+        </head>
           <body>
             You searched for:  """ + request.args["search_term"] + """ <br>
             This feed has: """ + print_item_length + """ items. <br>
-            """ + str(f) + """
+            """ + entries + """
           </body>
         </html>"""
     return html
-    
+
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8080) 
+    app.run(debug=False, port=8080)
     # Don't use Debug=True in Wing, is seems to not release the socket and you'll get a bind error when you run it next!
     print("done")
-
-
